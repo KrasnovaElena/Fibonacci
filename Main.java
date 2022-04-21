4d01cbdbe064e50ffcaf62ec4cc6b885b26af45d
KrasnovaElena <89896503+KrasnovaElena@users.noreply.github.com>
2022-04-21 18:17:42 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {/*Create function fib that returns n'th element of Fibonacci sequence (classic programming task).*/
+    public class Fibonacci {
+        public static long fib (int n) {
+            if (n <= 1) {
+                return n;
+            }
+            return fib(n - 1) + fib(n -2);
+        }
+    }
+}
